2a25d293c363570d965a13d62aeda35115b348e1
Brijendra Baghel <baghelbrijendra7@gmail.com>
2026-01-10 23:09:21 +0530

    Added notes as comment

--- python_crash_course/chapter_03_lists/bicycles.py
+++ python_crash_course/chapter_03_lists/bicycles.py
@@ -9,3 +9,23 @@ print(bicycles[0])
 #presentable by using the title() method:
 
 print(bicycles[0].title())
+
+#Index Positions Start at 0, Not 1
+
+print(bicycles[1])
+
+print(bicycles[3])
+
+"""Python has a special syntax for accessing the last element
+
+in a list. If you ask for the item at index -1, Python always
+
+returns the last item in the list:"""
+
+print(bicycles[-1])
+
+#Using Individual Values from a List
+
+message = f"My first bicycle was a {bicycles[0].title()}."
+
+print(message) 
