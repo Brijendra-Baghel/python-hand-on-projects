7164c8b606cec91c5e2bd91445f07cfdb0d23bc2
Brijendra Baghel <baghelbrijendra7@gmail.com>
2026-01-10 23:03:59 +0530

    Learned  about list

---
+++ python_crash_course/chapter_03_lists/bicycles.py
@@ -0,0 +1,11 @@
+bicycles = ['trek', 'cannondale', 'redline', 'specialized']
+
+print(bicycles)
+
+#Accessing Elements in a List
+
+print(bicycles[0])
+
+#presentable by using the title() method:
+
+print(bicycles[0].title())
